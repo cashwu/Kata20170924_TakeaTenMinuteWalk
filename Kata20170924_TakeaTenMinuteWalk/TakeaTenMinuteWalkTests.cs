1bd397dd216cd7cf6190083cd681540d9f65bf5b
cash <cash@cashwu.com>
2017-09-24 16:27:20 +0800

    input w, e test case

--- Kata20170924_TakeaTenMinuteWalk/TakeaTenMinuteWalkTests.cs
+++ Kata20170924_TakeaTenMinuteWalk/TakeaTenMinuteWalkTests.cs
@@ -40,6 +40,12 @@ namespace Kata20170924_TakeaTenMinuteWalk
             IsValidWalkShouldTrue(new[] { "e", "w" });
         }
 
+        [TestMethod]
+        public void input_w_e_should_return_true()
+        {
+            IsValidWalkShouldTrue(new[] { "w", "e" });
+        }
+
         private void IsValidWalkShouldTrue(string[] walk)
         {
             Assert.IsTrue(new Kata().IsValidWalk(walk));
@@ -62,7 +68,8 @@ namespace Kata20170924_TakeaTenMinuteWalk
 
             if (walk[0] == "n" && walk[1] == "s" ||
                 walk[0] == "s" && walk[1] == "n" ||
-                walk[0] == "e" && walk[1] == "w")
+                walk[0] == "e" && walk[1] == "w" ||
+                walk[0] == "w" && walk[1] == "e")
             {
                 return true;
             }
