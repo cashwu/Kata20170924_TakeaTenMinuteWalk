e9d7497eb1671e3229bd36ac92dbc2b63e400362
cash <cash@cashwu.com>
2017-09-24 15:35:03 +0800

    input n test case

--- Kata20170924_TakeaTenMinuteWalk/TakeaTenMinuteWalkTests.cs
+++ Kata20170924_TakeaTenMinuteWalk/TakeaTenMinuteWalkTests.cs
@@ -6,9 +6,24 @@ namespace Kata20170924_TakeaTenMinuteWalk
     [TestClass]
     public class TakeaTenMinuteWalkTests
     {
+        //  n
+        //w   e
+        //  s
+
         [TestMethod]
-        public void TestMethod1()
+        public void input_n_should_return_false()
         {
+            var kata = new Kata();
+            var actual = kata.IsValidWalk(new[] {"n"});
+            Assert.IsFalse(actual);
+        }
+    }
+
+    public class Kata
+    {
+        public bool IsValidWalk(string[] walk)
+        {
+            return false;
         }
     }
 }
